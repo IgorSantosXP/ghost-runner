39a78ff781572c7ed24e7c60419ee857abc0480c
Igor Santos <igor_santosxp@hotmail.com>
2022-04-07 22:24:05 -0300

    Added jump / Added astronaut character / Added Run animation

--- Assets/Runtime/Scripts/PlayerController.cs
+++ Assets/Runtime/Scripts/PlayerController.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 { 
-    [SerializeField] private float forwardSpeed = 5f;
+    [SerializeField] private float forwardSpeed = 10f;
     [SerializeField] private float laneDistanceX = 1.5f;
     [SerializeField] private float horizontalSpeed = 10f;
 
+    [Header("Jump")]
+    [SerializeField] private float jumpDistanceZ = 5;
+    [SerializeField] private float jumpHeightY = 2;
+
+    [SerializeField] private float jumpLerpSpeed = 10;
+
     public Vector3 initialPosition { get; private set; }
+    public bool IsJumping { get; private set; }
     private float LeftLaneX => initialPosition.x - laneDistanceX;
     private float RightLaneX => initialPosition.x + laneDistanceX;
     private float targetPositionX;
+    private float jumpStartZ;
 
     private void Awake()
     {
@@ -27,26 +35,62 @@ public class PlayerController : MonoBehaviour
 
         position.z += forwardSpeed*Time.deltaTime;
         position.x = ProcessLaneMovement();
+        position.y = ProcessJump();
 
         transform.position = position;
     }
 
     private void ProcessInput()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
             targetPositionX -= laneDistanceX;
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
             targetPositionX += laneDistanceX;
+        }
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) && !IsJumping)
+        {
+            StartJump();
         }
 
         targetPositionX = Mathf.Clamp(targetPositionX, LeftLaneX, RightLaneX);
     }
 
+    private void StartJump()
+    {
+        IsJumping = true;
+        jumpStartZ = transform.position.z;
+    }
+
+    private void StopJump()
+    {
+        IsJumping = false;
+    }
+
     private float ProcessLaneMovement()
     {
         return Mathf.Lerp(transform.position.x, targetPositionX, horizontalSpeed*Time.deltaTime);
+    }
+
+    private float ProcessJump()
+    {
+        float deltaY = 0;
+        if (IsJumping)
+        {
+            float jumpCurrentProgress = transform.position.z - jumpStartZ;
+            float jumpPercent = jumpCurrentProgress / jumpDistanceZ;
+            if (jumpPercent >= 1)
+            {
+                StopJump();
+            }
+            else
+            {
+                deltaY = Mathf.Sin(Mathf.PI * jumpPercent) * jumpHeightY;
+            }
+        }
+        float targetPositionY = initialPosition.y + deltaY;
+        return Mathf.Lerp(transform.position.y, targetPositionY, Time.deltaTime * jumpLerpSpeed);
     }
 }
